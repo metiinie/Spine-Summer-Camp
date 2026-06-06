6e7412012c6aa2f3f1a1ab32e19a0f9b941cc90c
metiinie <abumahilkerim@gmail.com>
2026-06-06 22:29:44 +0300

    Build the project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { RegistrationsModule } from './registrations/registrations.module';
 import { UploadsController } from './uploads/uploads.controller';
 import { EmailsService } from './emails/emails.service';
 import { PrismaService } from './prisma.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     AuthModule,
     RegistrationsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
   ],
-  controllers: [UploadsController],
-  providers: [EmailsService, PrismaService],
+  controllers: [UploadsController, AppController],
+  providers: [EmailsService, PrismaService, AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,10 +8,10 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://spine-summer-camp.vercel.app',
+      'https://spine-summer-camp.onrender.com'
     ],
     credentials: true,
   });
-  app.useStaticAssets(join(__dirname, '..', 'public'));
   const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Backend running on port ${port}`);
